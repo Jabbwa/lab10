d1df4c8924213b5f3824758106effeb74c073d43
Jabbwa <sopida_kit@cmu.ac.th>
2021-12-13 15:01:03 +0700

    complete

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,7 +1,24 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int x, int y){
+	int N=x, M=y;
+	if (N>0 && M>0)
+	{
+		
+		for (int i = 0; i < N; i++)
+		{
+			for (int i = 0; i < M; i++)
+			{
+				cout << 'O';
+			}
+		
+			cout << '\n';
+		}
+
+	}else cout << "Invalid input";
+	
+}
 
 int main(){
 	
